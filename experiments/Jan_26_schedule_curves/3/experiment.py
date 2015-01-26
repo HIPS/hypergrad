fe9eaca24762e2e59ba24fc4c483e7c339d59428
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-01-26 16:35:42 -0500

    Got reasonable curves on logistic MNIST

--- experiments/Jan_26_schedule_curves/3/experiment.py
+++ experiments/Jan_26_schedule_curves/3/experiment.py
@@ -1,4 +1,4 @@
-"""Gradient descent to optimize minimizing a parabola."""
+"""Gradient descent to optimize everything"""
 import numpy as np
 import numpy.random as npr
 import pickle
@@ -6,67 +6,76 @@ from collections import defaultdict
 
 from funkyyak import grad, kylist
 
+from hypergrad.util import memoize
 from hypergrad.data import load_data_dicts
 from hypergrad.nn_utils import make_nn_funs, VectorParser, logit, inv_logit
 from hypergrad.optimizers import sgd4, rms_prop, adam
 
 # ----- Fixed params -----
-N_iters = 60
-dimension = 100
+layer_sizes = [784, 10]
+batch_size = 200
+N_iters = 30
+N_classes = 10
+N_train = 1000
+N_valid = 10**3
+N_tests = 10**3
+N_batches = N_train / batch_size
+#N_iters = N_epochs * N_batches
 # ----- Initial values of learned hyper-parameters -----
 init_log_L2_reg = 0.0
 init_log_alphas = -2.0
 init_invlogit_betas = inv_logit(0.9)
-init_log_param_scale = -np.Inf #2.0
+init_log_param_scale = -4.0
 # ----- Superparameters -----
-meta_alpha = 0.02
+meta_alpha = 0.04
 N_meta_iter = 100
 
-npr.seed(0)
+global_seed = npr.RandomState(3).randint(1000)
 
 def fill_parser(parser, items):
     partial_vects = [np.full(parser[name].size, items[i])
                      for i, name in enumerate(parser.names)]
     return np.concatenate(partial_vects, axis=0)
 
-def make_parabola(d):
-    parser = VectorParser()
-    parser.add_shape('weights', d)
-    dimscale = np.exp(np.linspace(-3, 3, d))
-    offset = npr.randn(d)
-
-    def loss(w, X=0.0, T=0.0, L2_reg=0.0):
-        return np.dot((w - offset) * dimscale, (w - offset))
-
-    return parser, loss
-
 def run():
-    parser, loss_fun = make_parabola(dimension)
+    train_data, valid_data, tests_data = load_data_dicts(N_train, N_valid, N_tests)
+    parser, pred_fun, loss_fun, frac_err = make_nn_funs(layer_sizes)
     N_weight_types = len(parser.names)
     hyperparams = VectorParser()
     hyperparams['log_L2_reg']      = np.full(N_weight_types, init_log_L2_reg)
     hyperparams['log_param_scale'] = np.full(N_weight_types, init_log_param_scale)
     hyperparams['log_alphas']      = np.full(N_iters, init_log_alphas)
-    hyperparams['invlogit_betas']  = np.full(N_iters, init_invlogit_betas)
 
+    hyperparams['invlogit_betas']  = np.full(N_iters, init_invlogit_betas)
+    #fixed_hyperparams = VectorParser()
+    #fixed_hyperparams['invlogit_betas']  = np.full(N_iters, init_invlogit_betas)
+
+    # TODO: memoize
     def primal_optimizer(hyperparam_vect, i_hyper):
+        def indexed_loss_fun(w, L2_vect, i_iter):
+            rs = npr.RandomState(npr.RandomState(global_seed + i_hyper).randint(1000))
+            seed = i_hyper * 10**6 + i_iter   # Deterministic seed needed for backwards pass.
+            idxs = rs.randint(N_train, size=batch_size)
+            return loss_fun(w, train_data['X'][idxs], train_data['T'][idxs], L2_vect)
+
         learning_curve = []
         def callback(x, i_iter):
-            learning_curve.append(loss_fun(x))
+            if i_iter % N_batches == 0:
+                learning_curve.append(loss_fun(x, **train_data))
 
         cur_hyperparams = hyperparams.new_vect(hyperparam_vect)
         W0 = fill_parser(parser, np.exp(cur_hyperparams['log_param_scale']))
-        W0 *= npr.RandomState(hash(i_hyper)).randn(W0.size)
+        W0 *= npr.RandomState(global_seed + i_hyper).randn(W0.size)
         alphas = np.exp(cur_hyperparams['log_alphas'])
         betas  = logit(cur_hyperparams['invlogit_betas'])
         L2_reg = fill_parser(parser, np.exp(cur_hyperparams['log_L2_reg']))
-        W_opt = sgd4(grad(loss_fun), kylist(W0, alphas, betas, L2_reg), callback)
+        W_opt = sgd4(grad(indexed_loss_fun), kylist(W0, alphas, betas, L2_reg), callback)
         callback(W_opt, N_iters)
         return W_opt, learning_curve
 
     def hyperloss(hyperparam_vect, i_hyper):
         W_opt, _ = primal_optimizer(hyperparam_vect, i_hyper)
-        return loss_fun(W_opt)
+        return loss_fun(W_opt, **valid_data)
     hyperloss_grad = grad(hyperloss)
 
     meta_results = defaultdict(list)
@@ -76,7 +85,9 @@ def run():
         cur_hyperparams = hyperparams.new_vect(hyperparam_vect.copy())
         for field in cur_hyperparams.names:
             meta_results[field].append(cur_hyperparams[field])
-        meta_results['train_loss'].append(loss_fun(x))
+        meta_results['train_loss'].append(loss_fun(x, **train_data))
+        meta_results['valid_loss'].append(loss_fun(x, **valid_data))
+        meta_results['tests_loss'].append(loss_fun(x, **tests_data))
         meta_results['learning_curves'].append(learning_curve)
 
     final_result = rms_prop(hyperloss_grad, hyperparams.vect,
@@ -103,7 +114,7 @@ def plot():
 
     ax = fig.add_subplot(212)
     ax.set_title('Meta learning curves')
-    losses = ['train_loss']
+    losses = ['train_loss', 'valid_loss', 'tests_loss']
     for loss_type in losses:
         ax.plot(results[loss_type], 'o-', label=loss_type)
     ax.set_xlabel('Meta iter number')
@@ -116,17 +127,15 @@ def plot():
     ax = fig.add_subplot(211)
     ax.set_title('Alpha learning curves')
     for i, y in enumerate(results['log_alphas']):
-        if i % 10 == 0:
-            ax.plot(y, 'o-', label="Meta iter {0}".format(i))
+        ax.plot(y, 'o-', label="Meta iter {0}".format(i))
     ax.set_xlabel('Primal iter number')
     ax.set_ylabel('Log alpha')
     ax.legend(loc=1, frameon=False)
 
     ax = fig.add_subplot(212)
     ax.set_title('Beta learning curves')
-    for i, y in enumerate(results['invlogit_betas']):
-        if i % 10 == 0:
-            ax.plot(y, 'o-')
+    for y in results['invlogit_betas']:
+        ax.plot(y, 'o-')
     ax.set_xlabel('Primal iter number')
     ax.set_ylabel('Inv logit beta')
     plt.savefig('alpha_beta_curves.png')
@@ -150,7 +159,7 @@ def plot():
     plt.savefig('scale_and_reg.png')
 
 if __name__ == '__main__':
-    #results = run()
-    #with open('results.pkl', 'w') as f:
-    #    pickle.dump(results, f)
+    results = run()
+    with open('results.pkl', 'w') as f:
+        pickle.dump(results, f)
     plot()
